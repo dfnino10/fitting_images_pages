a2f96570f672caa88fc61fb5f94bfa2abf88dc0f
dfnino10 <df.nino10@uniandes.edu.co>
2021-02-14 11:50:16 -0500

    Signin mockup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import UserGreeting from './components/UserGreeting/UserGreeting';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <UserGreeting />
-      <ImageLinkForm />
-      {/*<OutputSize /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  onRouteChange = (route) => {
+    if(route === 'signout'){
+      this.setState({isSignedIn: false});
+    }else if(route === 'home'){
+      this.setState({isSignedIn: true});
+    }
+    this.setState({route: route});
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Navigation onRouteChange={this.onRouteChange} isSignedIn={this.state.isSignedIn}/>
+
+        { this.state.route === 'home' ? 
+            <>
+              <Logo />
+              <UserGreeting />
+              <ImageLinkForm />
+            </>
+            : (
+              this.state.route === 'signin'?
+              <>
+                <Signin onRouteChange={this.onRouteChange}/>
+              </>:
+              <>
+                <Register onRouteChange={this.onRouteChange}/>
+              </>
+            )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
